ec1dbb1514fe3ffc8936641bb1df1bf991132c2a
aksun1 <aksunpostit@gmail.com>
2022-03-05 17:17:25 +0200

    Base for ML algorithms

--- analysis.py
+++ analysis.py
@@ -1,7 +1,12 @@
+import math
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
+from sklearn.feature_selection import RFE
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, precision_score, confusion_matrix
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC  # evaluation metrics
 weather_data_files = {
     "Lahti": "resources/lahti_laune.csv",
     "Oulu": "resources/oulu_pellonpää.csv",
@@ -15,7 +20,7 @@ weather_data_files = {
 
 slip_warnings = pd.read_json("resources/warnings.json")
 
-slip_warnings = slip_warnings.drop("updated_at", 1) # created_at == updated_at always, so remove duplication
+slip_warnings = slip_warnings.drop(columns="updated_at") # created_at == updated_at always, so remove duplication
 slip_warnings.columns = ["id","city","date"]
 # filter out unknown cities:
 analyzing_cities = ['Lahti', 'Oulu', 'Kuopio', 'Jyväskylä', 'Helsinki', 'Joensuu']
@@ -39,26 +44,54 @@ for city in analyzing_cities:
     # join datasets
     weather_data = weather_data.assign(warning_issued = weather_data.date.isin(slip_warnings[slip_warnings["city"] == city].date))
     # remove columns "year", "month", "day", "time_zone" that are not used 
-    data = weather_data.drop(['Year','m','d','Time zone'],axis=1)  #columns are axis 1
+    weather_data = weather_data.drop(columns=['Year','m','d','Time zone']) 
 
 
     dates = weather_data['date'].unique() 
-
+    feature_cols = ['Ground minimum temperature (degC)', 'Air temperature (degC)', 'Precipitation amount (mm)','Snow depth (cm)']
     # iterate through the list of dates for which we have weather recordings
     for date in dates:
-        datapoint = weather_data[(data['date']==date)]  # select weather recordinds corresponding at day "date"
+        prev_day_weather = weather_data[(weather_data['date']==date - np.timedelta64(1, 'D'))]  # select weather recordinds corresponding at day "date"
+        day_weather = weather_data[(weather_data['date']==date)]  # select weather recordinds corresponding at day "date"
 
-        row_f = datapoint[(datapoint.Time=='00:00')]    # select weather recording at time "01:00"
-        row_l = datapoint["warning_issued"]    # select weather recording at time "01:00"
-        if len(row_f)==1:
-            # TODO: figure out proper features.
-            feature = row_f['Air temperature (degC)'].to_numpy()[0]  # extract the temperature recording at "01:00" as feature
-            label = row_l    # extract the temperature recording at "12:00" as label
-            features.append(feature)                  # add feature to list "features"
+        learn_fe_values = []
+        for fe in feature_cols:
+            feature_column = day_weather[fe].to_numpy()
+            feature = None
+            # find first non-NaN feature observation from the day's observations
+            for feature_candidate in feature_column:
+                if not np.isnan(feature_candidate):
+                    feature = feature_candidate
+                    break
+            learn_fe_values.append(feature)
+        # for fe in feature_cols:
+        #     feature_column = prev_day_weather[fe].to_numpy()
+        #     feature = None
+        #     # find first non-NaN feature observation from the day's observations
+        #     for feature_candidate in feature_column:
+        #         if not np.isnan(feature_candidate):
+        #             feature = feature_candidate
+        #             break
+        #     learn_fe_values.append(feature)
+
+        if None not in learn_fe_values:
+            label = day_weather["warning_issued"].to_numpy()[0]    # the warning data is the same for the same date regardless of the index here
+            features.append(learn_fe_values)                  # add feature to list "features"
             labels.append(label)                      # add label to list "labels"
             m = m+1
 
 
-X_demo = np.array(features).reshape(m,1)  # convert a list of len=m to a ndarray and reshape it to (m,1)
-y_demo = np.array(labels) # convert a list of len=m to a ndarray 
+X = np.array(features).reshape(m,len(feature_cols))  # convert a list of len=m to a ndarray and reshape it to (m,1)
+y = np.array(labels) # convert a list of len=m to a ndarray 
 
+clf_1 = LogisticRegression()    # initialise a LogisticRegression classifier, use default value for all arguments
+
+clf_1.fit(X,y)       # fit cfl_1 to data 
+y_pred = clf_1.predict(X)   # compute predicted labels for training data
+accuracy = accuracy_score(y, y_pred) # compute accuracy on the training set
+
+print("accuracy of LogReg : ", accuracy)
+
+## sanity check
+precision = precision_score(y, y_pred)
+print("precision of LogReg : ", precision)
